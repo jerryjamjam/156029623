665856d8a34387fad80c345cce2ae778bf318116
jerryjamjam <156284707+jerryjamjam@users.noreply.github.com>
2025-08-27 19:08:54 +0000

    Wed, Aug 27, 2025, 7:08 PM +00:00

--- practice/practicevars.py
+++ practice/practicevars.py
@@ -4,8 +4,14 @@
 num1 = int(input("What number are you thinking of? "))
 num2 = int(input("What number are you thinking of? "))
 
-add = num1 - num2
+add = num1 + num2
+subtract = num1 - num2
+multiply = num1 * num2
+divide = num1 / num2
+floordivide = num1 // num2
+modulus = num1 % num2
+exponential = num1 ** num2
 
-print(add)
+
 
 
