3313373265219ef3f3ca5e5fb6928a1dc3338536
jerryjamjam <156284707+jerryjamjam@users.noreply.github.com>
2025-08-27 18:21:23 +0000

    Wed, Aug 27, 2025, 6:21 PM +00:00

--- practice/practicevars.py
+++ practice/practicevars.py
@@ -1,8 +1,12 @@
-x = 2.3
-y = 1232
-z = "alice"
 
-print(type(x))
-print(type(y))
-print(type(z))
 
+name = "Jared"
+
+age = 32
+
+input = input("Do you like Python? ")
+if input == "yes":
+    input = "Love"
+
+
+print(f"Hello, {name}! You are {age} and {input} Python!")
