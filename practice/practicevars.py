71347f7f598757d96b1da5ec69a56c8c898ea60e
jerryjamjam <156284707+jerryjamjam@users.noreply.github.com>
2025-08-27 18:45:34 +0000

    Wed, Aug 27, 2025, 6:45 PM +00:00

--- practice/practicevars.py
+++ practice/practicevars.py
@@ -1,13 +1,13 @@
 
 
-name = "Jared"
-
-age = 32
-
+name = input("What is your name? ")
+age = int(input("What is your age? "))
 answer = input("Do you like Python? ").lower()
+
 if answer in ["yes", "yeah", "ye", "duh"]:
     answer = "love"
-elif answer in ["nah", "no", "fuck off"]:
-    answer = "hate"
+else:
+    if answer in ["nah", "no", "fuck off"]:
+        answer = "hate"
 
 print(f"Hello, {name}! You are {age} and {answer} Python!")
