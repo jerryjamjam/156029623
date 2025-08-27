0b4173d9b9ddcd96ad7eaf41b6a36ed78942edbc
jerryjamjam <156284707+jerryjamjam@users.noreply.github.com>
2025-08-27 19:36:43 +0000

    Wed, Aug 27, 2025, 7:36 PM +00:00

--- practice/practicevars.py
+++ practice/practicevars.py
@@ -11,7 +11,7 @@ Subtracting one from the other gives: {num1 - num2}
 Multiplying both gives: {num1 * num2}
 Dividing one from another gives: {num1 / num2}
 Using // gives: {num1 // num2}
-Getting the modulus from two numbers gives: {num1 & num2}
+Getting the modulus from two numbers gives: {num1 % num2}
 Powering one number by the other gives: {num1 ** num2}
 """
 print(message)
