ca134656bc7d470f5bd58c1578fe942cb8d207c0
jerryjamjam <156284707+jerryjamjam@users.noreply.github.com>
2025-08-27 19:32:12 +0000

    Wed, Aug 27, 2025, 7:32 PM +00:00

--- practice/practicevars.py
+++ practice/practicevars.py
@@ -9,10 +9,10 @@ message = f"""
 Adding both gives: {num1 + num2}
 Subtracting one from the other gives: {num1 - num2}
 Multiplying both gives: {num1 * num2}
-Dividing one from another gives: {divide}
-Using // gives: {floordivide}
-Getting the modulus from two numbers gives: {modulus}
-Powering one number by the other gives: {exponential}
+Dividing one from another gives: {num1 / num2}
+Using // gives: {num1 // num2}
+Getting the modulus from two numbers gives: {num1 & num2}
+Powering one number by the other gives: {num1 ** num2}
 """
 print(message)
 
