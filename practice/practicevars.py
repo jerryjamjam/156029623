f38a66433737df3fe3a1a1b3e65d73293f748908
jerryjamjam <156284707+jerryjamjam@users.noreply.github.com>
2025-08-27 19:02:17 +0000

    Wed, Aug 27, 2025, 7:02 PM +00:00

--- practice/practicevars.py
+++ practice/practicevars.py
@@ -4,12 +4,12 @@
 num1 = input("What number are you thinking of? ")
 num2 = input("What number are you thinking of? ")
 
-print(f"{num1} + {num2}")
-print(f"{num1} - {num2}")
-print(f"{num1} * {num2}")
-print(f"{num1} / {num2}")
-print(f"{num1} // {num2}")
-print(f"{num1} % {num2}")
-print(f"{num1} ** {num2}")
+print(int(num1 + num2))
+print(int(num1 - num2))
+print(int(num1 * num2))
+print(int(num1 / num2))
+print(int(num1 // num2))
+print(int(num1 % num2))
+print(int(num1 ** num2))
 
 
