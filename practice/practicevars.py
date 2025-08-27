2c42ea9d5fd5e7dd3305ee8522fd1d33955e29b6
jerryjamjam <156284707+jerryjamjam@users.noreply.github.com>
2025-08-27 19:30:11 +0000

    Wed, Aug 27, 2025, 7:30 PM +00:00

--- practice/practicevars.py
+++ practice/practicevars.py
@@ -4,29 +4,16 @@
 num1 = int(input("What's the first number? "))
 num2 = int(input("What's the second number? "))
 
-add = num1 + num2
-subtract = num1 - num2
-multiply = num1 * num2
-if num2 == 0:
-    print("cannot divide by 0")
-else:
-    divide = num1 / num2
-    floordivide = num1 // num2
-if num2 == 0:
-    print("can't use modulus with 0")
-else:
-    modulus = num1 % num2
-
-exponential = num1 ** num2
 
 message = f"""
-Adding both gives: {add}
-Subtracting one from the other gives: {subtract}
-Multiplying both gives: {multiply}
+Adding both gives: {num1 + num2}
+Subtracting one from the other gives: {num1 - num2}
+Multiplying both gives: {num1 * num2}
 Dividing one from another gives: {divide}
 Using // gives: {floordivide}
 Getting the modulus from two numbers gives: {modulus}
 Powering one number by the other gives: {exponential}
 """
 print(message)
+
 
