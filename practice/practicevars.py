ca88ef57c6c67661d4211f88fb2201ba1db65dc4
jerryjamjam <156284707+jerryjamjam@users.noreply.github.com>
2025-08-27 18:36:35 +0000

    Wed, Aug 27, 2025, 6:36 PM +00:00

--- practice/practicevars.py
+++ practice/practicevars.py
@@ -5,7 +5,7 @@ name = "Jared"
 age = 32
 
 answer = input("Do you like Python? ").lower()
-if answer == "yes" | "yeah" | "jeff":
+if answer == "yes" or "yeah" or "jeff":
     answer = "love"
 else:
     answer = "hate"
