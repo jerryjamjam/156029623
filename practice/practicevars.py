7934ca8282d2c9712c5a62f02d5f6ec43e0d9807
jerryjamjam <156284707+jerryjamjam@users.noreply.github.com>
2025-08-28 17:15:34 +0000

    Thu, Aug 28, 2025, 5:15 PM +00:00

--- practice/practicevars.py
+++ practice/practicevars.py
@@ -3,15 +3,17 @@
 
 num1 = int(input("What's the first number? "))
 num2 = int(input("What's the second number? "))
-
+divide = "cannot divide by 0" if num2 == 0 else f"Dividing one from another gives: {num1 / num2}"
+floordivide = "cannot divide by 0" if num2 == 0 else f"using // gives: {num1 // num2}"
+modulus = "cannot do modulus with num2 being 0" if num2 == 0 else f"getting the modulus from two numbers gives: {num1 % num2}"
 
 message = f"""
 Adding both gives: {num1 + num2}
 Subtracting one from the other gives: {num1 - num2}
 Multiplying both gives: {num1 * num2}
-Dividing one from another gives: {num1 / num2}
-Using // gives: {num1 // num2}
-Getting the modulus from two numbers gives: {num1 % num2}
+{divide}
+{floordivide}
+{modulus}
 Powering one number by the other gives: {num1 ** num2}
 """
 print(message)
