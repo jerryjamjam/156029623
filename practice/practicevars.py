f90c2c6a63219c2bf415e2449a5584c2292f24d5
jerryjamjam <156284707+jerryjamjam@users.noreply.github.com>
2025-08-28 18:26:02 +0000

    Thu, Aug 28, 2025, 6:26 PM +00:00

--- practice/practicevars.py
+++ practice/practicevars.py
@@ -12,13 +12,17 @@ if age <= 13:
     print("Sorry, you're too young for this movie")
 elif age in [13, 14, 15, 16, 17]:
     if studentid == "Y":
-        print("You get a student discount! Total")
+        print("You get a student discount!")
+        print(tickets * (teenprice * discount))
     else:
         print("Regular teen ticket price applies")
+
 elif age >= 18:
     if studentid == "Y":
         print("You get a student discount on the adult ticket!")
+
     else:
-        print("Full adult ticket price")
+        print("Full adult ticket price applies")
+
 
 
