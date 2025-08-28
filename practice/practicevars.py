68727e0e06477a7bcde9d3e7e555ff706b1702b0
jerryjamjam <156284707+jerryjamjam@users.noreply.github.com>
2025-08-28 19:31:56 +0000

    Thu, Aug 28, 2025, 7:31 PM +00:00

--- practice/practicevars.py
+++ practice/practicevars.py
@@ -1,11 +1,14 @@
+import sys
 
-age = int(input("How old are you? "))
-id = input("Do you have an ID? Y/N ").upper()
 vip = input("Are you a VIP memeber? Y/N ").upper()
-
 if vip == "Y":
     print("GET IN HERE!")
-elif age >= 21 and id == "Y":
+    sys.exit
+age = int(input("How old are you? "))
+id = input("Do you have an ID? Y/N ").upper()
+
+
+if age >= 21 and id == "Y":
     print("You may enter!")
 elif age <21 or id == "N":
     print("GET OUT OF HERE!")
