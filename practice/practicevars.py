918537287c8dacdeb4fa73769d63ee5be3a2cd75
jerryjamjam <156284707+jerryjamjam@users.noreply.github.com>
2025-08-28 19:15:25 +0000

    Thu, Aug 28, 2025, 7:15 PM +00:00

--- practice/practicevars.py
+++ practice/practicevars.py
@@ -1,32 +1,6 @@
 
 
 
-age = int(input("What is your age? "))
-studentid = input("Do you have a student ID? Y/N ").upper()
-tickets = int(input("how many tickets do you want? "))
 
-adultprice = 12
-teenprice = 10
-discount = .2
-baseprice = adultprice if age >=18 else teenprice
-total = baseprice * tickets
-subtotal = total - (total * discount)
-
-if age <= 12:
-    print("Sorry, you're too young for this movie")
-elif age in [13, 14, 15, 16, 17]:
-    if studentid == "Y":
-        print("You get a student discount!")
-        print(f"Total cost = ${subtotal:.2f}")
-    else:
-        print("Regular teen ticket price applies")
-        print(f"Total cost = ${total:.2f}")
-elif age >= 18:
-    if studentid == "Y":
-        print("You get a student discount on the adult ticket!")
-        print(f"Total cost = ${subtotal:.2f}")
-    else:
-        print("Full adult ticket price applies")
-        print(f"Total cost = ${total:.2f}")
 
 
