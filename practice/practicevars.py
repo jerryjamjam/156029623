a8b2aeb3b851cfc1b849cb1e93a3beafdec4a986
jerryjamjam <156284707+jerryjamjam@users.noreply.github.com>
2025-08-28 18:02:47 +0000

    Thu, Aug 28, 2025, 6:02 PM +00:00

--- practice/practicevars.py
+++ practice/practicevars.py
@@ -1,9 +1,15 @@
 
 
 age = input("What is your age? ")
-studentid = input("Do you have a student ID? ")
+studentid = input("Do you have a student ID? Y/N ").upper()
 
 if age <= 13:
     print("Sorry, you're too young for this movie")
-if age 
+if age in [13, 14, 15, 16, 17]:
+    if studentid == "Y":
+        print("You get a student discount")
+    else:
+        print("Regular teen ticket price applies")
+if age >= 18:
+    
 
