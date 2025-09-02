4a7e461d88171e14ad0258acc4b2abdb80517539
jerryjamjam <156284707+jerryjamjam@users.noreply.github.com>
2025-09-02 16:15:51 +0000

    Tue, Sep 2, 2025, 4:15 PM +00:00

---
+++ practice/practiceops.py
@@ -0,0 +1,30 @@
+
+
+
+
+
+
+
+
+age = int(input("How old are you? "))
+id = input("Do you have an ID? Y/N ").upper()
+vip = input("Are you a VIP memeber? Y/N ").upper()
+
+if vip == "Y": print("GET IN HERE!")
+
+elif age >= 21 and id == "Y": print("You may enter!")
+
+elif age <21 or id == "N": print("GET OUT OF HERE!")
+
+second optoin is
+import sys
+
+vip = input("Are you a VIP memeber? Y/N ").upper()
+if vip == "Y": print("GET IN HERE!")
+    sys.exit
+    age = int(input("How old are you? "))
+if age == "N": print("GET OUT OF HERE!")
+    sys.exit
+    id = input("Do you have an ID? Y/N ").upper()
+if id == "N": print("GET OUT OF HERE!")
+    sys.exit
