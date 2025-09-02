8bc27877570e8e7a5fdfb718507013720fbbda4d
jerryjamjam <156284707+jerryjamjam@users.noreply.github.com>
2025-09-02 16:56:59 +0000

    Tue, Sep 2, 2025, 4:56 PM +00:00

--- practice/practicevars.py
+++ practice/practicevars.py
@@ -1,45 +1,22 @@
 num1 = int(input("First Number? "))
 num2 = int(input("Second Number? "))
-operation = input("Choose an Operation (+, -, /, *, //, %, **) ")
+operation = input("Choose an Operation (+, -, /, //, %, *, **) ")
 
 operations = {
-    
+    "+" = num1 + num2
+    "-"
+    "/"
+    "//"
+    "%"
+    "*"
+    "**"
 }
+
+
+result = 0
 
 if operation == "+":
     result = num1 + num2
-
-elif operation == "-":
-    result = num1 - num2
-
-elif operation == "*":
-    result = num1 * num2
-
-elif operation == "/":
-    if num2 == 0:
-        result = "Cannot divide by zero"
-    else:
-        result = num1 / num2
-
-elif operation == "//":
-    if num2 == 0:
-        result = "Cannot // by zero"
-    else:
-        result = num1 // num2
-
-elif operation == "%":
-    if num2 == 0:
-        result = "Cannot modulus by zero"
-    else:
-        result = num1 % num2
-
-elif operation == "**":
-    result = num1 ** num2
-
-else:
-    print("can't do that")
-
-if num2 == 0 and operation in [/, //, %]:
     print(f"You chose {num1} {operation} {num2} -> Error: {result}")
 else:
     print(f"You chose {num1} {operation} {num2} -> Result: {result}")
