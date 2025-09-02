6d3bc4062a0402c6d9e197db61a7b5b7ad498295
jerryjamjam <156284707+jerryjamjam@users.noreply.github.com>
2025-09-02 17:10:50 +0000

    Tue, Sep 2, 2025, 5:10 PM +00:00

--- practice/practicevars.py
+++ practice/practicevars.py
@@ -4,25 +4,25 @@ operation = input("Choose an Operation (+, -, /, //, %, *, **) ")
 
 
 def add(num1, num2):
-    num1 + num2
+    return num1 + num2
 
 def sub(num1, num2):
-    num1 - num2
+    return num1 - num2
 
 def divide(num1, num2):
-    num1 / num2
+    return num1 / num2
 
 def floordivide(num1, num2):
-    num1 // num2
+    return num1 // num2
 
 def modulus(num1, num2):
-    num1 % num2
+    return num1 % num2
 
 def times(num1, num2):
-    num1 * num2
+    return num1 * num2
 
 def expo(num1, num2):
-    num1 ** num2
+    return num1 ** num2
 
 operations = {
     "+": add,
@@ -34,7 +34,7 @@ operations = {
     "**": expo
 }
 
-result = operations[{operation}](num1, num2)
+result = operations[operation](num1, num2)
 
 if num2 == 0 and operation in ["/", "//", "%"]:
     print(f"You chose {num1} {operation} {num2} -> Error: {result}")
