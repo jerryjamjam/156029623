796fce82f73122b3b32d098220f9257c67bd4a64
jerryjamjam <156284707+jerryjamjam@users.noreply.github.com>
2025-09-02 15:53:16 +0000

    Tue, Sep 2, 2025, 3:53 PM +00:00

--- practice/practicevars.py
+++ practice/practicevars.py
@@ -5,27 +5,28 @@ num1 = int(input("Second Number? "))
 operation = input("Choose an Operation (+, -, /, *, //, %, **)")
 
 if operation == "+":
-    plus = num1 + num2
+    operation = num1 + num2
 
 elif operation == "-":
-    minus = num1 + num2
+    operation = num1 - num2
 
 elif operation == "*":
-    times = num1 + num2
+    operation = num1 * num2
 
 elif operation == "/":
-    divide = num1 + num2
-    
+    operation = num1 / num2
 
 elif operation == "//":
-    floordivide = num1 + num2
+    operation = num1 // num2
 
 elif operation == "%":
-    modulus = num1 + num2
+    operation = num1 % num2
 
 elif operation == "**":
-    kwargs = num1 + num2
+    operation = num1 ** num2
 
 else:
     print("can't do that")
 
+
+print(f"You chose {num1} {operation} {num2})
