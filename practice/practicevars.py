e99f91e4083042c3d11b6e381842ccc36b11807e
jerryjamjam <156284707+jerryjamjam@users.noreply.github.com>
2025-09-02 15:49:17 +0000

    Tue, Sep 2, 2025, 3:49 PM +00:00

--- practice/practicevars.py
+++ practice/practicevars.py
@@ -6,9 +6,25 @@ operation = input("Choose an Operation (+, -, /, *, //, %, **)")
 
 if operation == "+":
     plus = num1 + num2
+
 elif operation == "-":
     minus = num1 + num2
-    
+
+elif operation == "*":
+    times = num1 + num2
+
+elif operation == "/":
+    divide = num1 + num2
+
+elif operation == "//":
+    floordivide = num1 + num2
+
+elif operation == "%":
+    modulus = num1 + num2
+
+elif operation == "**":
+    kwargs = num1 + num2
+
 else:
     print("can't do that")
 
