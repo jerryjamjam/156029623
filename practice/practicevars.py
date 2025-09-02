3af4db9b5afd66a1038350a750f181c01be896bf
jerryjamjam <156284707+jerryjamjam@users.noreply.github.com>
2025-09-02 16:41:37 +0000

    Tue, Sep 2, 2025, 4:41 PM +00:00

--- practice/practicevars.py
+++ practice/practicevars.py
@@ -3,38 +3,36 @@ num2 = int(input("Second Number? "))
 operation = input("Choose an Operation (+, -, /, *, //, %, **)")
 
 if operation == "+":
-    operation = num1 + num2
+    result = num1 + num2
 
 elif operation == "-":
-    operation = num1 - num2
+    result = num1 - num2
 
 elif operation == "*":
-    operation = num1 * num2
+    result = num1 * num2
 
 elif operation == "/":
     if num2 == 0:
-        print("Cannot divide by zero")
+        result = "Cannot divide by zero"
     else:
-        operation = num1 / num2
+        result = num1 / num2
 
 elif operation == "//":
     if num2 == 0:
-        print("Cannot // by zero")
+        result = "Cannot // by zero"
     else:
-        operation = num1 // num2
+        result = num1 // num2
 
 elif operation == "%":
     if num2 == 0:
-        print("Cannot modulus by zero")
-    operation = num1 % num2
+        result = "Cannot modulus by zero"
+    result = num1 % num2
 
 elif operation == "**":
-    operation = num1 ** num2
+    result = num1 ** num2
 
 else:
     print("can't do that")
 
-if num2 == 0:
-    print(f"You chose {num1} {operation} {num2} =")
-else:
-    print(f"You chose {num1} {operation} {num2}NO")
+print(f"You chose {num1} {operation} {num2} = {result}")
+
