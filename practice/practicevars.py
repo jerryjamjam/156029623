1f173ee7581aea9b09a79d11dfd72177fb503484
jerryjamjam <156284707+jerryjamjam@users.noreply.github.com>
2025-09-02 15:45:22 +0000

    Tue, Sep 2, 2025, 3:45 PM +00:00

--- practice/practicevars.py
+++ practice/practicevars.py
@@ -4,7 +4,10 @@ num1 = int(input("First Number? "))
 num1 = int(input("Second Number? "))
 operation = input("Choose an Operation (+, -, /, *, //, %, **)")
 
-if operation in [+, -, /, *, //, %, **]:
+if operation == "+":
+    plus = num1 + num2
+elif operation == "-":
+    minus = num1 + num2
     
 else:
     print("can't do that")
