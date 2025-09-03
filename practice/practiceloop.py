8a4d11f4dbf0829e8f74612ec6a8da41787635d6
jerryjamjam <156284707+jerryjamjam@users.noreply.github.com>
2025-09-03 16:10:44 +0000

    Wed, Sep 3, 2025, 4:10 PM +00:00

--- practice/practiceloop.py
+++ practice/practiceloop.py
@@ -1,10 +1,2 @@
-for row in range(3):       # Outer loop (rows)
-    for col in range(3):   # Inner loop (columns)
-        print(f"({row}, {col})", end=" ")
-    print()
+#hiiiiiii
 
-
-
-(0, 0) (0, 1) (0, 2)
-(1, 0) (1, 1) (1, 2)
-(2, 0) (2, 1) (2, 2)
